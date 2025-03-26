16c3023f75c145dc3c3730a12f122f48f9666067
Redar13321 <r_e_d_a_r@vk.com>
2025-03-26 14:39:58 +0500

    dz

---
+++ PrimeNumber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class PrimeNumber
+    {
+        public int MaxIterrations = 10000; // int.MaxValue
+        public PrimeNumber() { }
+        public IEnumerable<int> GetPrimes(int limit)
+        {
+            int n = 2;
+
+            while (n < limit)
+            {
+                if (IsPrime(n))
+                {
+                    yield return n;
+                }
+                n++;
+            }
+        }
+        public bool IsPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public IEnumerable<int> GetAllPrimes()
+        {
+            return GetPrimes(MaxIterrations);
+        }
+
+        public IEnumerable<int> SkipPrimes(int count)
+        {
+            int n = count;
+            int MaxIterrations = this.MaxIterrations;
+            while (n < MaxIterrations)
+            {
+                if (IsPrime(n))
+                {
+                    yield return n;
+                }
+                n++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumber primNum = new();
+
+            foreach (var arg in primNum.GetPrimes(50))
+            {
+                Console.Write($"{arg}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var arg in primNum.SkipPrimes(400))
+            {
+                Console.Write($"{arg}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var arg in primNum.GetAllPrimes())
+            {
+                Console.Write($"{arg}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
