83e8aefe9f83f02a4e169f5eaa28c03cc0373ebe
Redar13321 <r_e_d_a_r@vk.com>
2025-05-29 15:37:38 +0500

    init 0.5

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			//object e;
+			//e = listToDo;
+			//e = groupBoxToDo;
+			//e = titleToDo;
+			//e = dateToDo;
+			//e = descriptionToDo;
+			//e = buttonAdd;
+
+			InitializeComponent();
+
+			listToDo.ItemsSource = new List<ToDo>()
+			{
+				new("job one", "guaguaguaguaguaguagua"),
+				new("job two", "guaguaguaguaguaguagua"),
+				new("job three", "guaguaguaguaguaguagua"),
+			};
+		}
+
+        private void toogleVisiblityToDo_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (buttonAdd == null || groupBoxToDo == null) return;
+			CheckBox checkBox = (CheckBox)sender;
+            bool v = checkBox.IsChecked == true;
+            groupBoxToDo.Visibility = v ? Visibility.Visible : Visibility.Hidden;
+            buttonAdd.Visibility = groupBoxToDo.Visibility;
+        }
+    }
+}
---
+++ ToDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp9
+{
+    public class ToDo
+    {
+        public string Name { get => _name; }
+        public string? Description { get => _description; }
+        public DateOnly Date { get => _date; }
+        public ToDo(string name, string? Description, DateOnly? date = null)
+        {
+            this._name = name;
+            this._description = Description;
+            this._date = date ?? DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        string _name;
+        string? _description;
+        DateOnly _date;
+    }
+}
