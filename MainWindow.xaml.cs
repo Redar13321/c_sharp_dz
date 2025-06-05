59b8f40e29559e2af8431e12783aced1af789f57
Redar13321 <r_e_d_a_r@vk.com>
2025-06-05 15:30:16 +0500

    init0.5

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+            ListBoxToDo.ItemsSource = new List<ToDo>();
+            updateBarToDo();
+        }
+
+		public void ButtonDelete_Clicked(object sender, RoutedEventArgs e)
+		{
+            if (ListBoxToDo.SelectedIndex == -1)
+                return;
+            List<ToDo> list = (List<ToDo>)ListBoxToDo.ItemsSource;
+            list.RemoveAt(ListBoxToDo.SelectedIndex);
+            ListBoxToDo.ItemsSource = null;
+            ListBoxToDo.ItemsSource = list;
+            updateBarToDo();
+            //ListBoxToDo.Items.RemoveAt(ListBoxToDo.SelectedIndex);
+        }
+		public void ButtonNewToDo_Clicked(object sender, RoutedEventArgs e)
+		{
+			Window win = new NewToDoWindow();
+			win.Owner = this;
+			win.Show();
+		}
+
+        public void AddNewToDo(ToDo toDo)
+        {
+            List<ToDo> list = (List<ToDo>)ListBoxToDo.ItemsSource;
+            list.Add(toDo);
+            ListBoxToDo.ItemsSource = null;
+            ListBoxToDo.ItemsSource = list;
+            updateBarToDo();
+        }
+
+        public void onSetToDoIsDone(object sender, RoutedEventArgs e)
+        {
+            updateBarToDo();
+        }
+
+        void updateBarToDo()
+		{
+			if (ListBoxToDo.Items.Count > 0)
+			{
+                textBarToDO.Visibility = progBarToDO.Visibility = Visibility.Visible;
+				progBarToDO.Maximum = ListBoxToDo.Items.Count;
+				progBarToDO.Value = (ListBoxToDo.ItemsSource as List<ToDo>).Count(i=>i.Done);
+				textBarToDO.Text = $"{progBarToDO.Value}/{progBarToDO.Maximum}";
+            }
+			else
+            {
+                textBarToDO.Visibility = progBarToDO.Visibility = Visibility.Collapsed;
+            }
+		}
+	}
+}
---
+++ ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    public class ToDo
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public bool Done { get; set; }
+        public DateTime Date { get; set; }
+        public ToDo(string name, string? Description, bool? Done = false, DateTime? date = null)
+        {
+            this.Name = name;
+            this.Description = Description;
+            this.Done = Done ?? false;
+            this.Date = date ?? DateTime.Now;
+        }
+
+    }
+}
